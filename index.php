f2fd06032526fe99740be2e9ddb4d3c5eb751279
matheus souza <ruivorobado@gmail.com>
2020-11-16 16:51:42 -0300

    adicionando o arquivo php com estrutura basica de html

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="" id="">
+        <input type="text" name="" id="">
+        <input type="submit" value="enviar">
+    </form>
+    
+</body>
+</html>
